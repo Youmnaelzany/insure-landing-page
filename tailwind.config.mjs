b9b861ef12060f4102c2692409bc3befd540c877
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-04-06 23:08:30 +0200

    setting up the working space
    add Astro project
    add React & Tailwind
    add folder & Files for the project
    remove node_modules and package.jason and install bun

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    fontFamily: {
+      Karla: ["Karla", "sans-serif"],
+      "DM-Serif-Display": ["DM Serif Display", "serif"],
+    },
+
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
